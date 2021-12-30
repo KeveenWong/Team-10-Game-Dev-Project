6aff55321e7643df59c87b15c544879d3727e217
KeveenWong <67565283+KeveenWong@users.noreply.github.com>
2021-12-29 23:33:59 -0800

    initial commit with eye components; tracking not yet implemented

---
+++ PASSIVE CREATURES/Eye.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var mouse_position = get_viewport().get_mouse_position() + camera.global_position
+	eye.rotation = (mouse_position - eye.global_position).angle()
